1f991ff656c9fbe185b4420aa6fe7562d65cccd6
Lukas Domagala <lukas.domagala@brandung.de>
2021-11-18 15:28:37 +0100

    added flamegraph screenshot

--- dev/user.cljs
+++ dev/user.cljs
@@ -2,33 +2,35 @@
   (:require [omni-trace.omni-trace :as o]
             [omni-trace.testing-ns :as e]
             [omni-trace.flamegraph :as flame]
-            [portal.web :as p]
-            [cljs.pprint :as pp]))
+            [portal.web :as p]))
 
-(.log js/console "test")
-
-(def ^{:a 5} test-var  1)
 
 (comment
+  ;instrument a namespace
+  (o/instrument-ns 'omni-trace.testing-ns)
+  ;run functions in that namespace
+  (-> e/machine-init
+      (e/insert-coin :quarter)
+      (e/insert-coin :dime)
+      (e/insert-coin :nickel)
+      (e/insert-coin :penny)
+      (e/press-button :a1))
+  ;look at traces for every function that was traced
+  @o/workspace
+  ;connect to portal
   (def portal (p/open))
   (add-tap #'p/submit)
-  (o/instrument-ns 'omni-trace.testing-ns)
-  (.log js/console (-> e/machine-init
-                       (e/insert-coin :quarter)
-                       (e/insert-coin :dime)
-                       (e/insert-coin :nickel)
-                       (e/insert-coin :penny)
-                       (e/press-button :a1)))
-  (pp/pprint @o/workspace)
-  
-  (tap>(flame/flamegraph (flame/flamedata @o/workspace)))
+  ;send the trace to portal as a vegajs flamegraph
+  (tap> (flame/flamegraph (flame/flamedata @o/workspace)))
+  ;remove tracing from a namesapce
   (o/uninstrument-ns 'omni-trace.testing-ns)
 
   o/instrumented-vars
   (macroexpand '(o/uninstrument-ns 'omni-trace.testing-ns))
-  (alter-meta! (var omni-trace.testing-ns/insert-coin )assoc-in [:stuffs] "yeah123")
+  (alter-meta! (var omni-trace.testing-ns/insert-coin) assoc-in [:stuffs] "yeah123")
   (.log js/console (meta (var omni-trace.testing-ns/insert-coin)))
 
   ({1 :1 2 :2 3 :3} 1)
-  .)
+  .
+  )
   
